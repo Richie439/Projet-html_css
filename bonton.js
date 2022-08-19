11e7a07b8bc9d694ad786ebea93cead5030f4a8d
Richie <richierichard439@gmail.com>
2022-08-19 17:14:46 +0100

    committt

--- bonton.js
+++ bonton.js
@@ -6,4 +6,5 @@ for (var i=0;i<4;i++){
     console.log(block[i]) //sa permet d'afficher sur la console de navigateur
     block[i].classList.toggle("fond") // c'est pour ajouter ou enlever une class(si le fond n'existe pas met sinon enleve)
 }
-})
+}
+)
