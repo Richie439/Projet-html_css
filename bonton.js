ab3c9357bad06df7a0c1d41d38bbda1bea91262f
Richie <richierichard439@gmail.com>
2022-08-19 17:10:48 +0100

    comitttt

---
+++ bonton.js
@@ -0,0 +1,9 @@
+var bonton=document.getElementById("richie")
+var block=document.getElementsByClassName("bloc") // 
+//c'est qui permet d'écouteur un évenement
+bonton.addEventListener("click", function (){
+for (var i=0;i<4;i++){
+    console.log(block[i]) //sa permet d'afficher sur la console de navigateur
+    block[i].classList.toggle("fond") // c'est pour ajouter ou enlever une class(si le fond n'existe pas met sinon enleve)
+}
+})
